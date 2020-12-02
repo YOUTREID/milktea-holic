0156148d41ce56c72750f94bee6fbea47f9679a7
YOUTREID <johnnyzhou123>
2020-12-02 13:55:51 -0800

    Updates

---
+++ precache-manifest.18b6d4a048c0e41d031d650ea1a15d06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d13357f64c14db81d0bdcf905e0ebb",
+    "url": "/YOUTREID/milktea-holic/index.html"
+  },
+  {
+    "revision": "849998c4c3606203b0ba",
+    "url": "/YOUTREID/milktea-holic/static/css/2.b63eaa2c.chunk.css"
+  },
+  {
+    "revision": "2839b2fa24b92712d8d1",
+    "url": "/YOUTREID/milktea-holic/static/css/main.7a2e65e7.chunk.css"
+  },
+  {
+    "revision": "849998c4c3606203b0ba",
+    "url": "/YOUTREID/milktea-holic/static/js/2.c158411e.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/YOUTREID/milktea-holic/static/js/2.c158411e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2839b2fa24b92712d8d1",
+    "url": "/YOUTREID/milktea-holic/static/js/main.d149d979.chunk.js"
+  },
+  {
+    "revision": "635ad0746d3d126f19ae",
+    "url": "/YOUTREID/milktea-holic/static/js/runtime-main.9ab3d95f.js"
+  },
+  {
+    "revision": "90f6f1ad301ca65f39267d83c1debbb7",
+    "url": "/YOUTREID/milktea-holic/static/media/milktea.90f6f1ad.svg"
+  },
+  {
+    "revision": "bd8c49e939bda47d5608e53542cf3db3",
+    "url": "/YOUTREID/milktea-holic/static/media/p1.bd8c49e9.jpg"
+  },
+  {
+    "revision": "679ab11abaf30db2d028628b4948911a",
+    "url": "/YOUTREID/milktea-holic/static/media/p2.679ab11a.jpg"
+  },
+  {
+    "revision": "46d5369f5216103b640b61a8489a9838",
+    "url": "/YOUTREID/milktea-holic/static/media/p3.46d5369f.jpg"
+  }
+]);
